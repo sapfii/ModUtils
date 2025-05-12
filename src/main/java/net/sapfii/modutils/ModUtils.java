f30a6cf2cee166607b4b3923cab7db9b08a9304e
sapfii <159391078+sapfii@users.noreply.github.com>
2025-05-11 21:02:02 -0400

    Inital Commit

---
+++ src/main/java/net/sapfii/modutils/ModUtils.java
@@ -0,0 +1,12 @@
+package net.sapfii.modutils;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ModUtils implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+        
+    }
+
+}
